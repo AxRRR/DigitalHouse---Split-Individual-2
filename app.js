52b3931bd8dfb814e6bd940d3146bfd6f34526e5
AxRRR <alexwrot@gmail.com>
2022-01-11 12:00:28 -0600

    Navigation finished

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
 const express = require('express');
+require('dotenv').config();
 const path = require('path');
 const app = express();
 
-// app.use(express.static(path.join(__dirname, '/public')));
-
-// app.use('/css',express.static(__dirname +'.public/css'));
 app.use(express.static(path.join(__dirname, './public')));
 
 app.get('/inicio', (req, res) => {
     res.sendFile((__dirname + '/views/home.html'));
 });
 
-// app.use(express.static(__dirname + '/public'));
-// app.use(express.static(__dirname + './public'));
-
-// app.use(express.static(path.join(__dirname, './public')));
-
-app.listen(4000, () =>  {
-    console.log('[Express]: Se ha iniciado correctamente en el puerto 4000.')
+app.listen(process.env.PORT, () =>  {
+    console.log('[Express]: Se ha iniciado correctamente en el puerto: ' + process.env.PORT)
 });
