1e8b9d6446cda6869e9994ec7e091fca0df78178
AxRRR <alexwrot@gmail.com>
2022-01-18 20:37:39 -0600

    Mobile version finished

--- public/scripts/button.js
+++ public/scripts/button.js
@@ -1,19 +1,17 @@
-// const buttonClick = document.getElementById('mobile_button').innerText;
+
 document.getElementById("mobileButton").addEventListener("click", () => showMenu());
 
 let menuValue = false;
 function showMenu(){
     if(!menuValue){
-        document.getElementById("menuUser1").className = 'navigation--user visibilityEnable';
-        document.getElementById("menuUser2").className = 'navigation--categories visibilityEnable';
-        document.getElementById("menuInput").className = 'navigation--search visibilityEnable';
-        console.log(menuValue);
+        document.getElementById("menuUser1").className = 'navigation__user visibilityEnable';
+        document.getElementById("menuUser2").className = 'navigation__categories visibilityEnable';
+        document.getElementById("menuInput").className = 'navigation__search visibilityEnable';
         return menuValue = true;
     }
-    console.log(menuValue);
-    document.getElementById("menuUser1").className = 'navigation--user visibilityDisable';
-    document.getElementById("menuUser2").className = 'navigation--categories visibilityDisable';
-    document.getElementById("menuInput").className = 'navigation--search visibilityDisable';
+    document.getElementById("menuUser1").className = 'navigation__user visibilityDisable';
+    document.getElementById("menuUser2").className = 'navigation__categories visibilityDisable';
+    document.getElementById("menuInput").className = 'navigation__search visibilityDisable';
     menuValue = false;
 }
 
