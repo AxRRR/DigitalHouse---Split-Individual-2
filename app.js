8ddaeb2e8f2ad0cd0eea2188534444889f689321
AxRRR <alexwrot@gmail.com>
2022-01-27 23:56:24 -0600

    Animations and grid incorporated

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/inicio', (req, res) => {
+app.get('/home', (req, res) => {
     res.sendFile((__dirname + '/views/home.html'));
 });
 
